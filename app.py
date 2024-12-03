43af2daacc46f0973ac757c7c215e9f4c61cbbd3
Sanjeev-Coditation <sanjeev@coditation.com>
2024-12-03 14:21:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import logging
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy import inspect
+#from flask_sqlalchemy import SQLAlchemy
+#from werkzeug.security import generate_password_hash, check_password_hash
+#from sqlalchemy import inspect
 #from dotenv import load_dotenv
 from typing import Optional
 from flask_cors import CORS
@@ -15,18 +15,11 @@ CORS(app)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info(f'postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}')
+# logger.info(f'postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-# Define User model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -39,21 +32,11 @@ def register():
     username = data.get('username')
     password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
+    if username == "admin" and password == "admin":
 
-    # Check if the user already exists
-    existing_user = User.query.filter_by(username=username).first()
-    if existing_user:
-        return jsonify({'message': 'User exists'}), 409  # Conflict status code
-
-    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-    new_user = User(username=username, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'User registered successfully'}), 201
-
+        return jsonify({'message': 'User registered successfully'}), 201
+    else:
+        return jsonify({"message": "NOPE!"})
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -61,16 +44,10 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
-
-    user = User.query.filter_by(username=username).first()
-    if not user or not check_password_hash(user.password, password):
-        return jsonify({'message': 'Invalid credentials'}), 401
-
-    return jsonify({'message': 'Logged in successfully'}), 200
+    if username=="admin" and password=="admin":
+        return jsonify({'message': 'Logged in successfully'}), 200
+    else:
+        return jsonify({"message": "NOOOPE!"})
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  
     app.run(host='0.0.0.0', port=3656)
